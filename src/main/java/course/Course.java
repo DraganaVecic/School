a412f279666be2b112ef0d3f517774520ed9c0dc
DraganaVecic <dragana1986.r@gmail.com>
2020-02-26 19:10:09 +0100

    Created class Course

---
+++ src/main/java/course/Course.java
@@ -0,0 +1,46 @@
+
+package course;
+
+
+public class Course {
+    
+    private String name;
+    private int numberOfClasses;
+    private String codeName;
+    
+public Course() {
+    this.name = "";
+    this.numberOfClasses = 0;
+    this.codeName = "";
+}
+public void setName(String customName) {
+        this.name = customName;
+    }
+    public void setNumberOfClasses (int customNumberOfClasses) {
+        this.numberOfClasses = customNumberOfClasses;
+    }
+    public void setCodeName(String customCodeName) {
+        this.codeName = customCodeName;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    public int getNumberOfClasses() {
+        return this.numberOfClasses;
+    }
+    public String getCodeName() {
+        return this.codeName;
+    }
+    public Course(String customName, int customNumberOfClasses, String customCodeName){
+        this.name = customName;
+        this.numberOfClasses = customNumberOfClasses;
+        this.codeName = customCodeName;
+    }
+    public Course(String "QA", int 40, String "pet"){
+        this.name = "QA";
+        this.numberOfClasses = 40;
+        this.codeName = "pet";
+}
+    
+}
